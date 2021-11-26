735a2225dcf296efabe61e6484054370bd3d52e2
Administrator <mahdiakbari1683@gmail.com>
2021-11-26 18:22:58 +0330

    web middleware added to route

--- src/routes/route.php
+++ src/routes/route.php
@@ -1,4 +1,4 @@
 <?php
 
-Route::get('contact-form', [\MAAK\ContactForm\Http\Controllers\IndexController::class,'index']);
+Route::get('contact-form', [\MAAK\ContactForm\Http\Controllers\IndexController::class,'index'])->middleware('web');
 Route::post('contact-form', [\MAAK\ContactForm\Http\Controllers\IndexController::class,'send'])->name('maak.contactForm.send');
